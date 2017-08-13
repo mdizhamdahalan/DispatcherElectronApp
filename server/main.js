2a24b75d4f26cca6fa3b09502b6f130dc46181ad
saibot777 <stefantrajkovic777@gmail.com>
2017-08-14 00:22:59 +0200

    fixing the title

--- server/main.js
+++ server/main.js
@@ -8,8 +8,8 @@ app.on('ready', createWindow)
 
 function createWindow () {
     // Create the browser window
-    win = new BrowserWindow({width: 800, height: 600})
-
+    win = new BrowserWindow({width: 1200, height: 800, title: 'Dispatcher'})
+    
     // and load the index.html of the app.
     win.loadURL(url.format({
         pathname: path.join(__dirname, '../www/index.html'),
